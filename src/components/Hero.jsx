6ff0bb8bb9097c559ed5d72003bf6aa7bc65fc48
giorgos.L <giorgoslankonakes@gmail.com>
2025-01-24 20:35:48 +0200

    Update button again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -67,7 +67,7 @@ function Hero() {
             </motion.p>
             <motion.a
               variants={childVariants}
-              href="/Giorgos%20-%20Lagkonakis%20CV.pdf"
+              href="/Giorgos_Lagkonakis_CV"
               target="_blank"
               rel="nooper noreferrer"
               download
